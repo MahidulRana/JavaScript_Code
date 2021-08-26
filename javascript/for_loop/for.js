fbd9df33adf05ea4283d5ef4db7ae8b78b880137
MahidulRana <73422120+MahidulRana@users.noreply.github.com>
2021-08-27 00:34:59 +0600

    Add files via upload

---
+++ javascript/for_loop/for.js
@@ -0,0 +1,8 @@
+var m = parseInt(prompt("enter a number :"));
+var n = parseInt(prompt("enter a number :"));
+var sum =0;
+for (var x=m ; x<=n ; x++){
+    sum = sum+x;
+    
+}
+document.write(" " +sum);   
---
+++ javascript/for_loop/ternary.js
@@ -0,0 +1,4 @@
+var number = Number(prompt("enter a number :"));
+
+var result = number >0 ? "positive" : number <0 ? "negative" : "Zero";
+console.log(result);
